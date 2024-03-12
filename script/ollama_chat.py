432ff58307b6b9613ff16ab392484f06eb8b6131
sh-ikeda <sikeda@dbcls.rois.ac.jp>
2024-03-12 17:06:25 +0900

    update prompt

--- script/ollama_chat.py
+++ script/ollama_chat.py
@@ -12,23 +12,29 @@ You are given JSON-formatted data that describe a sample used in a biological ex
 These data are written by scientists who conducted the experiment. Since they are not as familiar with data structurization as you, these might be not structurized well.
 As a curator, extract some information described below from input data and output in JSON format.
 
-These are information you must extract from input data:
-1. tissue
+Follow the steps below to construct your output JSON.
+1. Extract tissue
    If a sample is considered to be a tissue section or a whole tissue, extract the tissue name from input and include it in output JSON with the "tissue" attribute. For example, if a sample is considered to be a section of liver, your output must include `"tissue": "liver"`.
-2. host tissue
-   Some biological samples are content of a tissue. For example, content of digestive tract can be used as a sample to investigate microbe of the tract.
-   If a sample is considered to be content collected from a tissue, extract the tissue name from input and include it in output JSON with the "host_tissue" attribute. For example, if a sample is considered to be content collected from intestine, your output must include `"host_tissue": "intestine"`.
-3. cell line
+   If you extract a tissue name, proceed to the step 5. Otherwise, proceed to the step 2.
+2. Extract host tissue
+   Some biological samples are content of a tissue. For example, content of digestive tract can be used as a sample to investigate microbe of the tract. If a sample is considered to be content collected from a tissue, extract the tissue name from input and include it in output JSON with the "host_tissue" attribute. For example, if a sample is considered to be content collected from intestine, your output must include `"host_tissue": "intestine"`.
+   If you extract a host tissue name, proceed to the step 5. Otherwise, proceed to the step 3.
+3. Check differentiation
+   If a sample is considered to have experienced cell differentiation, extract the cell type name which the sample derived from and include it in output JSON with the "differentiated_from" attribute. Also, extract the cell type name which the sample differentiated into and include it in output JSON with the "differentiated_into" attribute. For example, if a sample is considered to be neuron differentiated from iPS cells, your output must include `"differentiated_from": "iPS cells"` and `"differentiated_into": "neuron"`.
+   If you extract a cell name in this step, then proceed to the step 6. Otherwise, proceed to the step 4.
+4. Extract cell line
    A cell line is a group of cells that are genetically identical and have been cultured in a laboratory setting. For example, HeLa, Jurkat, HEK293, etc. are names of commonly used cell lines.
    If a sample is considered to be a cell line, extract the cell line name from input and include it in output JSON with the "cell_line" attribute. For example, if a sample is considered to be HeLa, your output must include `"cell_line": "HeLa"`.
    This attribute is supposed to describe a specific cell line name. Therefore, even if you find a string like "lung cancer cell line", you do not put the string in your output.
-4. cell type
+   If you extract a cell line name, proceed to the step 7. Otherwise, proceed to the step 5.
+5. Extract cell type
    If a sample is considered to be a specific type of cell, extract the cell type name from input and include it in output JSON with the "cell_type" attribute. For example, if a sample is considered to be leukocyte, your output must include `"cell_type": "leukocyte"`.
-   Note that some samples might be experienced or experiencing cell differentiation. In this case, "cell_type" attribute is supposed to be a name of resulting differentiated cell, rather than a name of a cell the sample derived from. For example, if a sample is considered to be neuron differentiated from iPS cells, your output must include `"cell_type": "neuron"`, rather than `"cell_type": "iPS cells"`.
-5. disease
+   Proceed to the step 6.
+6. Extract disease
    If input data mention a disease of the organism the sample derived, extract the disease name from input and include it in output JSON with the "disease" attribute. For example, if a sample is considered to be collected from a patient of amyotrophic lateral sclerosis, your output must include `"disease": "amyotrophic lateral sclerosis"`.
-
-Your output is a JSON-formatted data including all data you extracted. If you do not find information listed above in input data, you do not output the attribute. For example, if a sample is considered to be neuron collected from a patient of amyotrophic lateral sclerosis, your output is: {"cell_type": "neuron", "disease": "amyotrophic lateral sclerosis"}.
+   Proceed to the step 7.
+7. Output JSON
+   Your final output is a JSON-formatted data including all data you extracted. If you do not find information listed above in input data, you do not output the attribute. For example, if a sample is considered to be neuron collected from a patient of amyotrophic lateral sclerosis, your output is: {"cell_type": "neuron", "disease": "amyotrophic lateral sclerosis"}.
 
 Are you ready?'''
 
