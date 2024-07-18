7300c7bb81a268ff70db322f5b9cf601b08ff250
sh-ikeda <sikeda@dbcls.rois.ac.jp>
2024-07-18 11:33:18 +0900

    refactor: add review mode

--- bsllmner/__main__.py
+++ bsllmner/__main__.py
@@ -7,6 +7,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('mode', choices=["extract", "review"])
     parser.add_argument('input_filename', help='BioSample JSON file')
+    parser.add_argument('-r', '--metasra_tsv', help='MetaSRA output tsv file. Required for review mode.')
+    parser.add_argument('-l', '--llmner_tsv', help='Extraction output tsv file. Required for review mode.')
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-t', '--test', action='store_true')
     parser.add_argument('-m', '--model')
@@ -15,13 +17,19 @@ def main():
 
     mode = args.mode
     input_json = util.load_json(args.input_filename)
+    metasra_tsv = args.metasra_tsv
+    llmner_tsv = args.llmner_tsv
     verbose = args.verbose
     test = args.test
     model = args.model
     prompt_indices = args.prompt_indices.split(",")
     util.print_time()
     if mode == "extract":
-        ollama_chat.ollma_ner(input_json, model, prompt_indices, verbose, test)
+        bsner = ollama_chat.BsNer(input_json, model, prompt_indices)
+        bsner.ner(verbose, test)
+    elif mode == "review":
+        bsreview = ollama_chat.BsReview(input_json, model, prompt_indices, metasra_tsv, llmner_tsv)
+        bsreview.review(verbose, test)
     util.print_time()
     return
 
--- bsllmner/lib/ollama_chat.py
+++ bsllmner/lib/ollama_chat.py
@@ -2,41 +2,178 @@ import ollama
 import sys
 import json
 import re
+import os
+from collections import defaultdict
 from ..prompt import load_prompt
 from .util import print_time
 
 
-def ollama_ner(input_json, model, prompt_indices, verbose=False, test=False):
-    prompts = load_prompt.parse_md()
+class BsLlmProcess:
+    def __init__(self, bs_json, model, prompt_indices):
+        self.bs_json = bs_json
+        self.model = model
+        self.prompt_indices = prompt_indices
+        self.prompts = self.load_prompt()
+        return
 
-    to = 10 if test else len(input_json)
-    for i in range(0, to):
-        input_bs = json.dumps(input_json[i])
-        if i%10==0 and verbose and not test:
-            print_time(str(i))
+    def load_prompt(self):
+        fname = "prompt.md"
+        dirname = os.path.dirname(os.path.abspath(__file__))
+        prompts = {}
+        with open(dirname + "/" + fname, "r") as f:
+            current_id = ""
+            is_first_line = False
+            for line in f:
+                if line[0] == "#":
+                    current_id = re.sub("^# ", "", line).strip()
+                    prompts[current_id] = {}
+                    prompts[current_id]["text"] = ""
+                    is_first_line = True
+                elif is_first_line:
+                    prompts[current_id]["role"] = line.strip()
+                    is_first_line = False
+                elif current_id != "":
+                    prompts[current_id]["text"] += line
 
+        for id in prompts:
+            prompts[id]["text"] = prompts[id]["text"].strip()
+        return prompts
+
+    def construct_messages(self):
         messages = []
-        for j in range(0, len(prompt_indices)):
+        for j in range(0, len(self.prompt_indices)):
             messages.append({
-                "role": prompts[prompt_indices[j]]["role"],
-                "content": prompts[prompt_indices[j]]["text"]
+                "role": self.prompts[self.prompt_indices[j]]["role"],
+                "content": self.prompts[self.prompt_indices[j]]["text"]
             })
-        messages.append({"role": "user", "content": "\n" + input_bs})
-        options = {"temperature": 0}
-        response = ollama.chat(model=model, messages=messages, options=options)
-        res_text = response["message"]["content"]
+        return messages
 
+    def extract_json_from_llmout(self, llmout):
         res_text_json = ""
-        if "{" in res_text:
+        if "{" in llmout:
             try:
-                res_text_json = re.findall(r"\{[^}]*\}", res_text)[-1]
+                res_text_json = re.findall(r"\{[^}]*\}", llmout)[-1]
             except IndexError:
                 res_text_json = '{"error": "IndexError"}'
+        return res_text_json.replace("\n", "")
 
-        bs_id = input_json[i]["accession"]
-        print(bs_id, res_text_json.replace("\n",""), res_text.replace("\n", " "), sep="\t")
 
-    return
+class BsNer(BsLlmProcess):
+    def __init__(self, bs_json, model, prompt_indices):
+        super().__init__(bs_json, model, prompt_indices)
+        return
+
+    def ner(self, verbose=False, test=False):
+        to = 10 if test else len(self.bs_json)
+        messages_base = self.construct_messages()
+        for i in range(0, to):
+            input_bs = json.dumps(self.bs_json[i], indent=2)
+            if i%10==0 and verbose and not test:
+                print_time(str(i))
+
+            messages = messages_base
+            messages[-1]["content"] += input_bs
+            options = {"temperature": 0}
+            response = ollama.chat(model=self.model, messages=messages, options=options)
+            res_text = response["message"]["content"]
+
+            bs_id = self.bs_json[i]["accession"]
+            res_text_json = self.extract_json_from_llmout(res_text)
+            print(bs_id, res_text_json, res_text.replace("\n", " "), sep="\t")
+        return
+
+
+class BsReview(BsLlmProcess):
+    def __init__(self, bs_json, model, prompt_indices, metasra_tsv, llmner_tsv):
+        super().__init__(bs_json, model, prompt_indices)
+        self.metasra_tsv = metasra_tsv
+        self.llmner_tsv = llmner_tsv
+        self.llmner_dict =  self.parse_llmner_tsv()
+        self.bs_cvcl_cands = self.collect_bs_cvcl_cands()
+
+    def collect_bs_cvcl_cands(self):
+        bs_cvcl_count = defaultdict(int)
+        bs_cvcl_cands = defaultdict(list)
+        with open(self.metasra_tsv, "r") as f:
+            for line in f:
+                spline = line.split("\t")
+                bs_id = spline[0]
+                if len(spline) <= 3 :
+                    continue
+                ont_term_id = spline[3]
+                if ont_term_id.startswith("CVCL:"):
+                    bs_cvcl_count[bs_id] += 1
+                    bs_cvcl_cands[bs_id].append(json.loads(spline[9]))
+
+        for bs_id in bs_cvcl_count:
+            if bs_cvcl_count[bs_id] <= 1:
+                del bs_cvcl_cands[bs_id]
+        return bs_cvcl_cands
+
+    def parse_llmner_tsv(self):
+        llmner_dict = {}
+        with open(self.llmner_tsv) as f:
+            for line in f:
+                spline = line.split("\t")
+                bs_id = spline[0]
+                extracted_json = "{}"
+                if spline[1]:
+                    extracted_json = spline[1]
+                llmner_dict[bs_id] = {
+                    "extracted_json": json.loads(extracted_json),
+                    "full_output": spline[2].replace("  ", "\n ")
+                }
+        return llmner_dict
+
+    def reformat_cvcl(self, cvcl):
+        reformatted_dict = {}
+        reformatted_dict["id"] = cvcl["id"]
+        reformatted_dict["name"] = cvcl["name"]
+        for syn in cvcl["synonyms"]:
+            syn_type = syn["type"].lower() + "_synonyms"
+            if not syn_type in reformatted_dict:
+                reformatted_dict[syn_type] = []
+            reformatted_dict[syn_type].append(syn["name"])
+        reformatted_dict["xrefs"] = []
+        for i in range(0, len(cvcl["xrefs"])):
+            reformatted_dict["xrefs"].append(cvcl["xrefs"][i])
+            if cvcl["xrefs_comments"][i] != "":
+                reformatted_dict["xrefs"][-1] += " ! " + cvcl["xrefs_comments"][i]
+        return reformatted_dict
+
+    def review(self, verbose=False, test=False):
+        base_messages = self.construct_messages()
+        for bs in self.bs_json:
+            bs_id = bs["accession"]
+            if bs_id not in self.bs_cvcl_cands:
+                continue
+            messages = base_messages
+
+            ## Output from LLM in extraction
+            messages.insert(-1, {
+                "role": "assistant",
+                "content": self.llmner_dict[bs_id]["full_output"]
+            })
+
+            ## Input of BioSample json
+            messages[3]["content"] += json.dumps(bs, indent=2)
+
+            ## Candidate evaluation
+            ### Replace "{{cell_line}}" in the prompt with this cell line name
+            messages[-1]["content"] = messages[-1]["content"].replace("{{cell_line}}", "\""+self.llmner_dict[bs_id]["extracted_json"]["cell_line"]+"\"")
+            ### Input of candidate terms
+            for cvcl_cand in self.bs_cvcl_cands[bs_id]:
+                cvcl_id = cvcl_cand["id"].replace(":", "_")
+                messages[-1]["content"] += "\n```json: " + cvcl_id + ".json\n" + json.dumps(self.reformat_cvcl(cvcl_cand), indent=2) + "\n```\n"
+
+            ## Ask LLM
+            options = {"temperature": 0}
+            response = ollama.chat(model=self.model, messages=messages, options=options)
+            res_text = response["message"]["content"]
+            print("# ", bs_id)
+            print(res_text)
+
+        return
 
 
 if __name__ == "__main__":
