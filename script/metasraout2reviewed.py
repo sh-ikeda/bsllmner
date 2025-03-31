ae4806d0567131742bd92e0392a51b1ca66dcd96
sh-ikeda <sikeda@dbcls.rois.ac.jp>
2025-03-31 11:40:55 +0900

    allow jsonl as input

--- script/metasraout2reviewed.py
+++ script/metasraout2reviewed.py
@@ -1,5 +1,7 @@
 import json
 import argparse
+import sys
+
 
 def load_tsv(tsv_filename):
     tsv = []
@@ -7,6 +9,14 @@ def load_tsv(tsv_filename):
         for line in f:
             tsv.append(line.split("\t"))
     return tsv
+
+
+def load_json(json_filename):
+    jsonl = []
+    with open(json_filename, "r") as f:
+        for line in f:
+            jsonl.append(json.loads(line))
+    return jsonl
 
 
 def null2str(s):
@@ -18,9 +28,9 @@ def null2str(s):
         return s
 
 
-def parse_llmout(llmout):
+def parse_llmout_tsv(llmout_tsv):
     llm_conclusion = {}
-    for line in llmout:
+    for line in llmout_tsv:
         bs_id = line[0]
         llmout_json_str = line[1]
 
@@ -40,15 +50,29 @@ def parse_llmout(llmout):
     return llm_conclusion
 
 
+def parse_llmout_jsonl(llmout_jsonl):
+    llm_conclusion = {}
+    for line in llmout_jsonl:
+        bs_id = line["accession"]
+        llm_conclusion[bs_id] = line["output"]["cell_line_id"]
+    return llm_conclusion
+
 def select_metasraout(metasraout_filename, llm_conclusion):
+    checked_not_unique = set()
     with open(metasraout_filename, "r") as f:
         for line in f:
             spline = line.split("\t")
             bs_id = spline[0]
             if bs_id not in llm_conclusion:
                 print(line.strip())
-            elif llm_conclusion[bs_id][0:5] != "CVCL:":
-                print(line.strip())
+            elif llm_conclusion[bs_id][0:5] != "CVCL:":  # not unique
+                if bs_id not in checked_not_unique:
+                    for i in range(3, len(spline)):
+                        spline[i] = ""
+                    edited_line = "\t".join(spline)
+                    print(edited_line.strip())
+                    checked_not_unique.add(bs_id)
+                continue
             elif llm_conclusion[bs_id] == spline[3]:
                 print(line.strip())
     return
@@ -56,11 +80,18 @@ def select_metasraout(metasraout_filename, llm_conclusion):
 def main():
     parser = argparse.ArgumentParser(description="Terms not selected by LLM-review process is removed from metasraout tsv.")
     parser.add_argument('input_metasraout_filename', help='MetaSRA output tsv file')
-    parser.add_argument('input_llmout_review_filename', help='LLM output review tsv file')
+    parser.add_argument('input_llmout_review_filename', help='LLM output review file')
     args = parser.parse_args()
 
-    input_llmout = load_tsv(args.input_llmout_review_filename)
-    llm_conclusion = parse_llmout(input_llmout)
+    if args.input_llmout_review_filename.endswith(".tsv"):
+        input_llmout_tsv = load_tsv(args.input_llmout_review_filename)
+        llm_conclusion = parse_llmout_tsv(input_llmout_tsv)
+    elif args.input_llmout_review_filename.endswith(".jsonl"):
+        input_llmout_jsonl = load_json(args.input_llmout_review_filename)
+        llm_conclusion = parse_llmout_jsonl(input_llmout_jsonl)
+    else:
+        print("Error: Unsupported input file type. .tsv or .jsonl are allowed.", file=sys.stderr)
+        exit(1)
     select_metasraout(args.input_metasraout_filename, llm_conclusion)
     return
 
